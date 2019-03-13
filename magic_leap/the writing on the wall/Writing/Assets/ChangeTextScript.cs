f2b05e9e3344617d8c8e8fec75c3a90366b99af9
Lilika <lilika.markatou@gmail.com>
2019-03-13 15:08:13 -0400

    Add simple magic leap scene
    This scene displays text in front of the user. A script causes the text to change on runtime.

---
+++ magic_leap/the writing on the wall/Writing/Assets/ChangeTextScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TextMesh textObject = GameObject.Find("text").GetComponent<TextMesh>();
+        textObject.text = "test";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
