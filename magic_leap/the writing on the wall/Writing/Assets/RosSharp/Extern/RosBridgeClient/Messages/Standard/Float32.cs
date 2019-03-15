fc948d1ed96944be5776159c45f371ebc5b433d3
matthewberg <matthewoliverberg@gmail.com>
2019-03-15 14:08:32 -0400

    ros pub/sub support

---
+++ magic_leap/the writing on the wall/Writing/Assets/RosSharp/Extern/RosBridgeClient/Messages/Standard/Float32.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.Standard
+{
+    public class Float32 : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "std_msgs/Float32";
+        public float data;
+
+        public Float32()
+        {
+            data = 0;
+        }
+    }
+}
