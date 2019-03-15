93bb35f8a512b3c64bbbca889adcf16119b0990c
dwhit <david_whitney@brown.edu>
2019-03-15 11:28:57 -0400

    adding ros# functionality

---
+++ magic_leap/the writing on the wall/Writing/Assets/RosSharp/Extern/RosBridgeClient/Messages/MoveIt/AttachedCollisionObject.cs
@@ -0,0 +1,39 @@
+﻿/*
+© Siemens AG, 2017-2018
+Author: David Whitney (david_whitney@brown.edu)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using Newtonsoft.Json;
+
+namespace RosSharp.RosBridgeClient.Messages.MoveIt
+{
+    public class AttachedCollisionObject : Message
+    {
+        [JsonIgnore]
+        public const string RosMessageName = "moveit_msgs/AttachedCollisionObject";
+        public string link_name;
+        public MoveIt.CollisionObject obj;
+        public string[] touch_links;
+        public Trajectory.JointTrajectory detach_posture;
+        public float weight;
+        public AttachedCollisionObject()
+        {
+            link_name = "";
+            obj = new CollisionObject();
+            touch_links = new string[0];
+            detach_posture = new Trajectory.JointTrajectory();
+            weight = 0f;
+        }
+    }
+
+}
---
+++ magic_leap/the writing on the wall/Writing/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/PointPublisher.cs
@@ -0,0 +1,56 @@
+﻿/*
+Author: David Whitney (david_whitney@brown.edu)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PointPublisher : UnityPublisher<Messages.Geometry.Point>
+    {
+
+        public Transform PublishedTransform;
+        public string FrameId = "base";
+
+        private Messages.Geometry.Point message;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        private void Update()
+        {
+            UpdateMessage();
+        }
+
+        public void UpdateMessage()
+        {
+            message = GetGeometryPoint(PublishedTransform.localPosition.Unity2Ros());
+            Publish(message);
+        }
+
+        private Messages.Geometry.Point GetGeometryPoint(Vector3 position)
+        {
+            Messages.Geometry.Point geometryPoint = new Messages.Geometry.Point
+            {
+                x = position.x,
+                y = position.y,
+                z = position.z
+            };
+            return geometryPoint;
+        }
+    }
+}
