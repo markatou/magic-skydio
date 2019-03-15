3ec70a00b8814be2a1dc55cfa1a5de4330dde6d1
matthewberg <matthewoliverberg@gmail.com>
2019-03-15 11:49:41 -0400

    added ros# compatibility

---
+++ magic_leap/the writing on the wall/Writing/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/PoseStampedPublisher.cs
@@ -0,0 +1,78 @@
+﻿/*
+© Siemens AG, 2017-2018
+Author: Dr. Martin Bischoff (martin.bischoff@siemens.com)
+Edited By: David Whitney (david_whitney@brown.edu)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PoseStampedPublisher : UnityPublisher<Messages.Geometry.PoseStamped>
+    {
+        public Transform PublishedTransform;
+        public string FrameId = "map";
+
+        private Messages.Geometry.PoseStamped message;
+
+        protected override void Start()
+        {
+            base.Start();
+            InitializeMessage();
+        }
+
+        private void InitializeMessage()
+        {
+            
+            message = new Messages.Geometry.PoseStamped()
+            {
+                header = new Messages.Standard.Header()
+                {
+                    frame_id = FrameId
+                }
+            };
+        }
+
+        public void UpdateMessage()
+        {
+            message.header.Update();
+            message.pose.position = GetGeometryPoint(PublishedTransform.localPosition.Unity2Ros());
+            message.pose.orientation = GetGeometryQuaternion(PublishedTransform.localRotation.Unity2Ros());
+            Publish(message);
+        }
+
+        private Messages.Geometry.Point GetGeometryPoint(Vector3 position)
+        {
+            Messages.Geometry.Point geometryPoint = new Messages.Geometry.Point
+            {
+                x = position.x,
+                y = position.y,
+                z = position.z
+            };
+            return geometryPoint;
+        }
+
+        private Messages.Geometry.Quaternion GetGeometryQuaternion(Quaternion quaternion)
+        {
+            Messages.Geometry.Quaternion geometryQuaternion = new Messages.Geometry.Quaternion
+            {
+                x = quaternion.x,
+                y = quaternion.y,
+                z = quaternion.z,
+                w = quaternion.w
+            };
+            return geometryQuaternion;
+        }
+
+    }
+}
